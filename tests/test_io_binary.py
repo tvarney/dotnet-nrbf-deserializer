3bd1474bf17d429d33a5598c3a5dd92c42ae5f2f
tvarney <troy.a.varney@gmail.com>
2018-09-27 19:09:12 -0500

    Clarifying edits to the test_read() method

--- tests/test_io_binary.py
+++ tests/test_io_binary.py
@@ -9,29 +9,34 @@ from dotnet.primitives import *
 class TestBinaryFormatter(unittest.TestCase):
     def test_read(self):
         bf = BinaryFormatter()
-        missing_root_object_fp = io.BytesIO(b'\x00\x01\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x0B')
-        string_root_object_fp = io.BytesIO(b''.join((
-            b'\x00\x01\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00',
-            b'\x06\x01\x00\x00\x00\x0BHello World',
-            b'\x0B'
-        )))
-        extra_information_fp = io.BytesIO(string_root_object_fp.getvalue() + b'\x00\x00\x00\x00')
-        bad_header_record_id_fp = io.BytesIO(b'\x01\x00\x00\x00\x00\x00\x00\x00\x00')
-        bad_major_version_fp = io.BytesIO(b'\x00\x01\x00\x00\x00\x01\x00\x00\x00\x50\x00\x00\x00\x00\x00\x00\x00')
-        bad_minor_version_fp = io.BytesIO(b'\x00\x01\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x50\x00\x00\x00')
+
+        valid_header = b'\x00\x01\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00'
+        bin_string_record = b'\06\x01\x00\x00\x00\x0BHello World'
+        message_end = b'\x0B'
+
+        missing_root_object = b''.join((valid_header, message_end))
+        string_root_object = b''.join((
+            valid_header,
+            bin_string_record,
+            message_end
+        ))
+        extra_information = b''.join((valid_header, bin_string_record, message_end, b'\x00\x00\x00\x00'))
+        bad_header_record_id = b'\x01\x00\x00\x00\x00\x00\x00\x00\x00'
+        bad_major_version = b'\x00\x01\x00\x00\x00\x01\x00\x00\x00\x50\x00\x00\x00\x00\x00\x00\x00'
+        bad_minor_version = b'\x00\x01\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x50\x00\x00\x00'
 
         # Working cases
-        self.assertEqual(bf.read(string_root_object_fp)[0], "Hello World")
-        self.assertEqual(bf.read(extra_information_fp)[0], "Hello World")
+        self.assertEqual(bf.read(io.BytesIO(string_root_object))[0], "Hello World")
+        self.assertEqual(bf.read(io.BytesIO(extra_information))[0], "Hello World")
 
         # Missing record
-        self.assertRaises(KeyError, bf.read, missing_root_object_fp)
+        self.assertRaises(KeyError, bf.read, io.BytesIO(missing_root_object))
         # Bad header record byte
-        self.assertRaises(IOError, bf.read, bad_header_record_id_fp)
+        self.assertRaises(IOError, bf.read, io.BytesIO(bad_header_record_id))
         # Bad header major version
-        self.assertRaises(IOError, bf.read, bad_major_version_fp)
+        self.assertRaises(IOError, bf.read, io.BytesIO(bad_major_version))
         # Bad header minor version
-        self.assertRaises(IOError, bf.read, bad_minor_version_fp)
+        self.assertRaises(IOError, bf.read, io.BytesIO(bad_minor_version))
 
     def test_read_char(self):
         bf = BinaryFormatter()
